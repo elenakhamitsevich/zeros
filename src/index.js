e46da7f4c21e45ac5feb4897733a2ff0ca3f47ce
Elena Khamitsevitch <elena_khamitsevitch@mail.ru>
2018-09-30 21:21:45 +0300

    7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+i=1;
+count=0;
+while (number/Math.pow(5,i)>=1){
+  count = count + Math.floor(number/Math.pow(5,i));
+  i++;
+}
+return count;
 }
